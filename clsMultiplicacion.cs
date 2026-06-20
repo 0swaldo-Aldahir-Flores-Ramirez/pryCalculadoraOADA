406cfbc593566b053db68493d29c5baae25fddd5
Oswaldo Aldahir Flores Ramirez <20250994@uthh.edu.mx>
2026-06-19 21:17:15 -0600

    Se agrego la clase funcional Multiplicacion

---
+++ clsMultiplicacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryCalculadoraOADA
+{
+    internal class clsMultiplicacion : clsOperacion
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }      
+
+        public override double CalcularOperacion()
+        {
+            return Numero1 * Numero2;
+        }
+    }
+}
--- frmMultiplicacion.cs
+++ frmMultiplicacion.cs
@@ -12,9 +12,28 @@ namespace pryCalculadoraOADA
 {
     public partial class frmMultiplicacion : Form
     {
+        // Creacion de Obejeto
+        private clsMultiplicacion multi;
+
         public frmMultiplicacion()
         {
             InitializeComponent();
+            multi = new clsMultiplicacion();
+        }
+        // Evento para calcular la operacion
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            multi.Numero1 = double.Parse(txtNumero1.Text.Trim());
+            multi.Numero2 = double.Parse(txtNumero2.Text.Trim());
+
+            lblResultado.Text = "Resultado: " + multi.CalcularOperacion();
+            LimpiarCajas();
+        }
+        // Metodo para limpiar las cajas
+        private void LimpiarCajas()
+        {
+            txtNumero1.Clear();
+            txtNumero2.Clear();
         }
     }
 }
